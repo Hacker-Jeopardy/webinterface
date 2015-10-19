8c20ffcfb6a81100a080682feef01cb9fc2e292c
Simon Wörner <mail@simon-woerner.de>
2015-10-19 18:15:37 +0200

    playing around with webpack and co.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 // css
+require('../node_modules/purecss/build/base.css');
+require('../node_modules/purecss/build/grids.css');
+require('../node_modules/purecss/build/forms.css');
+require('../node_modules/purecss/build/buttons.css');
 require("../node_modules/font-awesome/less/font-awesome.less");
 require("./css/fonts.css");
 require("./css/jeopardy.css");
