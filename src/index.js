05364859eaa410067f18088f8ffa354b80dad482
Simon Wörner <git@simon-woerner.de>
2016-09-14 15:34:25 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import App from './app';
 import routes from './routes';
 import reducers from './reducers/index';
 import Layout from './components/layout';
-import persistState from 'redux-localstorage';
 
 export const initialState = {
   title: 'Jeopardy Webinterface',
   config: {},
 };
 
-const enhancers = {
-  persistState: persistState('/config'),
-};
+const enhancers = [];
 
 App({ reducers, enhancers, initialState, Layout, routes }).render();
--- tests/containers/nav_test.js
+++ tests/containers/nav_test.js
@@ -5,15 +5,19 @@ import routes from '../../src/routes';
 import Nav from '../../src/containers/nav';
 
 describe('<Nav />', () => {
+  let component;
+  const title = 'Title';
+
+  beforeEach(() => {
+    component = renderComponent(Nav, null, { title: title });
+  });
 
   it('contains the page title from supplied state', () => {
-    const wrapper = renderComponent(Nav, { title: 'Test' }, { title: 'Test' });
-    expect(wrapper.find('.navbar-brand').text()).to.equal('Test');
+    expect(component.find('.navbar-brand').text()).to.equal(title);
   });
 
   it('renders <Link> elements for all routes', () => {
-    const wrapper = renderComponent(Nav, { title: '' }, { title: '' });
-    expect(wrapper.find(Link)).to.have.length(routes.length);
+    expect(component.find(Link)).to.have.length(routes.length);
   });
 
 });
